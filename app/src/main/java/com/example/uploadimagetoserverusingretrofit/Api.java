efba18a6eaebe0d095fd9d8faf7598ce3f1d4123
ramimhossain12 <ramimhossain6242@gmail.com>
2021-03-25 23:07:54 +0600

    Api calling 1

--- app/src/main/java/com/example/uploadimagetoserverusingretrofit/Api.java
+++ app/src/main/java/com/example/uploadimagetoserverusingretrofit/Api.java
@@ -1,4 +1,17 @@
 package com.example.uploadimagetoserverusingretrofit;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface Api {
+
+    @FormUrlEncoded
+    @POST("")
+    Call<ResrponseModel> uploadImage(
+
+         @Field("EN_IMAGE") String encodeImage
+
+    );
 }
