82c71460a81487a2678d4f32485b2186957507d7
ramimhossain12 <ramimhossain6242@gmail.com>
2021-03-25 22:49:30 +0600

    image chooser

--- app/src/main/java/com/example/uploadimagetoserverusingretrofit/MainActivity.java
+++ app/src/main/java/com/example/uploadimagetoserverusingretrofit/MainActivity.java
@@ -1,19 +1,65 @@
 package com.example.uploadimagetoserverusingretrofit;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView ;
     private Button btnSelect, btnUpload;
+    private  int IMG_REQUEST = 21;
+
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.imageView);
+        btnSelect = findViewById(R.id.btnSelectImage);
+        btnUpload = findViewById(R.id.btnUploadImage);
+
+
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, IMG_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == IMG_REQUEST && resultCode == RESULT_OK && data != null){
+
+            Uri path = data.getData();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),path);
+
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
